fd552589a10c7175e10dc26537f64557f50c31b0
DARSHAN-BAMANIYA <162026275+DARSHAN-BAMANIYA@users.noreply.github.com>
2024-08-07 23:58:01 +0530

    Add files via upload

---
+++ HTML/java.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>example of js</title>
+	<script>
+		
+
+    document.write{"<h1> hello </h1>"};
+	document.Write {"example of js <hr>"};
+     var A1=60;  var B2=60;
+     var ans=A1+B2;
+	</script>
+</head>
+<body> 
+<h1>example of java</h1>
+</body>
+</html>
